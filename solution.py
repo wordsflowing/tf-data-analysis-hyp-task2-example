a9c578d20598eb651d6526f5f69a1822a39821e5
wordsflowing <129582590+wordsflowing@users.noreply.github.com>
2023-04-11 12:55:57 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind, mannwhitneyu
 
 
 chat_id = 422119389 # Ваш chat ID, не меняйте название переменной
@@ -8,4 +9,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, p = ttest_ind(x, y)
+    return p < 0.08 # Ваш ответ, True или False
