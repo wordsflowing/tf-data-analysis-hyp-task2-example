2b82d46be1d438dae34a3ebaf070b6f3310b77a0
wordsflowing <129582590+wordsflowing@users.noreply.github.com>
2023-04-11 16:23:56 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind, mannwhitneyu
+from scipy import stats
 
 
 chat_id = 422119389 # Ваш chat ID, не меняйте название переменной
@@ -9,5 +10,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    stat, p = ttest_ind(x, y)
+    stat, p = stats.ks_2samp(x, y)
     return p < 0.08 # Ваш ответ, True или False
